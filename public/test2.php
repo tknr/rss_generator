102353c15d289ab23ff63e2c12454e46a453c751
tk.hirai <tk.hirai@crusis.co.jp>
2022-08-16 11:46:27 +0900

    date

--- public/test2.php
+++ public/test2.php
@@ -48,26 +48,27 @@ $channel
 $list = $dom->find('div.recommendationList div.container div.__homel div.__item');
 foreach ($list as $content) {
 
-  $element = $content->find('div.__r div.__time')->innerHtml;
-  $element2 = $content->find('div.__r div.__time i')->outerHtml;
-  $element3 = $content->find('div.__r div.__time div')->outerHtml;
-  $element = str_replace($element2." ",'',$element);
-  $element = str_replace($element3,'',$element);
-  $date = DateTime::createFromFormat('H:i m-d-Y',$element);
-  var_dump($date);
+  $pubDate = 0; {
+    $element = $content->find('div.__r div.__time')->innerHtml;
+    $element2 = $content->find('div.__r div.__time i')->outerHtml;
+    $element3 = $content->find('div.__r div.__time div')->outerHtml;
+    $element = str_replace($element2 . " ", '', $element);
+    $element = str_replace($element3, '', $element);
+    $date = DateTime::createFromFormat('H:i m-d-Y', $element);
+    $pubDate = $date->getTimestamp();
+  }
 
-  
+
 
   $item  = new Item();
   $item
-  ->title($content->find('div.__l a')->getAttribute('title'))
-  ->description($content->find('div.__l a')->getAttribute('title'))
-  ->url($content->find('div.__l a')->getAttribute('href'))
-  ->enclosure($content->find('div.__l img')->getAttribute('src'))
-  ->pubDate($date->getTimestamp())
-//  ->content('<article><title>My first post</title><div id="content">Hello! I like sweets like Cupcake, Donut, Eclair, Froyo, Gingerbread, and so on...</div></atricle>')
-  ->appendTo($channel);
-  
+    ->title($content->find('div.__l a')->getAttribute('title'))
+    ->description($content->find('div.__l a')->getAttribute('title'))
+    ->url($content->find('div.__l a')->getAttribute('href'))
+    ->enclosure($content->find('div.__l img')->getAttribute('src'))
+    ->pubDate($pubDate)
+    //  ->content('<article><title>My first post</title><div id="content">Hello! I like sweets like Cupcake, Donut, Eclair, Froyo, Gingerbread, and so on...</div></atricle>')
+    ->appendTo($channel);
 }
 
 

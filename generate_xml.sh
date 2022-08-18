911eae5bde0017f8176efb794301854f561e4660
tknr <geishagirlskoume@gmail.com>
2022-08-18 17:25:00 +0900

    2022年  8月 18日 木曜日 17:25:00 JST

--- generate_xml.sh
+++ generate_xml.sh
@@ -2,12 +2,15 @@
 cd `dirname $0`
 export IFS=$'\n'
 
+RSS_LIST=''
 for php in `find ./public -iname "*.php"`;do
 	echo ${php}
 	xml=`basename ${php} | sed -e "s|\.php||g"`
 	echo ${xml}
 	php ${php} > public/static/${xml}.xml
+	RSS_LIST+="<li><a href='static/${xml}.xml'>${xml}</li>\n";
 done
+cat src/index.html | sed -e "s|#RSS_LIST#|${RSS_LIST}|g" > public/index.html
 
 git add --all || exit 1
 git commit -m "`date`" || exit 1
